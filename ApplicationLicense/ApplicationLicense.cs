8be77cc057ac78b8015cc9b50d72b027293f1a6a
Mykola Kovshar <knikolay20031230@gmail.com>
2022-10-29 12:55:50 +0300

    Add ApplicationLicense project

---
+++ ApplicationLicense/ApplicationLicense.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLicense
+{
+    public class ApplicationLicense
+    {
+        private readonly string proKey = "ProKey";
+        private readonly string trialKey = "TrialKey";
+        private readonly string key;
+
+        public ApplicationLicense(string key) => this.key = key;
+
+        private void ThrowELicenseError(string name) => throw new Exception($"Ви не можете використовувати дану версію.({name})");
+
+        public void AllowTrial()
+        {
+            if (key == trialKey)
+                Console.WriteLine("Тріальний режим");
+            else ThrowELicenseError("Trial");
+        }
+
+        public void AllowPro()
+        {
+            if (key == proKey)
+                Console.WriteLine("Платна версія");
+            else ThrowELicenseError("Pro");
+        }
+
+        public void AllowCommon()
+        {
+            Console.WriteLine("Безкоштовна версія");
+        }
+
+    }
+}
---
+++ ApplicationLicense/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLicense
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            ApplicationLicense license = new ApplicationLicense("ProKey");
+            PrintLicense(license);
+        }
+
+        private static void PrintLicense(ApplicationLicense license)
+        {
+            try
+            {
+                license.AllowPro();
+                license.AllowTrial();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                license.AllowCommon();
+            }
+        }
+    }
+}
